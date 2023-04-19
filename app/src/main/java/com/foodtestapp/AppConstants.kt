b1ffa13ffd52d30130c19dd79c4666a292d2a9a2
gouravspyne <gourav.dhankher@spyne.ai>
2023-04-19 15:49:01 +0530

    first commit

---
+++ app/src/main/java/com/foodtestapp/AppConstants.kt
@@ -0,0 +1,300 @@
+package com.foodtestapp
+
+
+object AppConstants {
+
+
+    val SHOOT_TYPE: String ="shoot_type"
+    val UPLOAD_IMAGE_LIST: String = "UPLOAD_IMAGE_LIST"
+    val REMAINING_SHOOT: String = "remaining_shoot"
+    val MAX_SHOOT: String = "max_shoot"
+    val ENTITY_PROJECTS: String = "entity_project"
+    val DEALERSHIP_NAME: String = "DEALERSHIP_NAME"
+    val DEALERSHIP_ID: String = "DEALERSHIP_ID"
+    val IS_ADD_IMAGE = "IS_ADD_IMAGE"
+    val BACKGROUND_ID = "BACKGROUND_ID"
+    val FROM_DEEPLINK = "FROM_DEEPLINK"
+    val BACKGROUND_NAME = "BACKGROUND_NAME"
+    val USER_ID = "USER_ID"
+    val CHECK_QC = "CHECK_QC"
+    val ENTITY_NAME = "ENTITY_NAME"
+    val ID = "ID"
+    val PHONE_NUMBER: String = "email_id"
+
+    val STATUS = "STATUS"
+    val IS_SKU_DATA_SENT = "IS_SKU_DATA_SENT"
+
+    const val BASE_IMAGE_URL_FOOD = "https://storage.googleapis.com/"
+
+    val SYNC_TYPE = "SYNC_TYPE"
+    val ENTITY_ID = "ENTITY_ID"
+    val PROCESS_SKU = "PROCESS_SKU"
+
+    val DEFAULT_BG_ID = "DEFAULT_BG_ID"
+    val SHOOT_DAO_TAG = "ShootDao"
+    val IS_SKU_IMAGE_UPDATED = "is_sku_image_updated"
+    val SERVICE_STARTED_BY = "SERVICE_STARTED_BY"
+    val SESSION_ID: String = "SESSION_ID"
+    val ADLOID: String = "Adloid"
+    val ENTERPRISE_ID: String = "ENTERPRISE_ID"
+    val SPYNE_ENTERPRISE_ID: String = "TaD1VC1Ko"
+    val FLIPKART_ENTERPRISE_ID: String = "EVV1XVY4R"
+    val KARVI_ENTERPRISE_ID: String = "DaD1JC1Fp"
+    val OLA_ENTERPRISE_ID: String = "3HOBWP4KN"
+    val ZOOMCAR_ENTERPRISE_ID: String = "P6LCAHA3Z"
+    val BASE_URL: String = "https://beta-api.spyne.xyz/"
+//        val BASE_URL: String =  "https://www.clippr.ai/"
+    val STATUS_PROJECT_NAME: String = "STATUS_PROJECT_NAME"
+    val SETTING_STATUS_GYRO: String = "STATUS_SETTING_GYRO"
+    val SETTING_STATUS_GRID: String = "STATUS_SETTING_GRID"
+    val SETTING_STATUS_OVERLAY: String = "STATUS_SETTING_OVERLAY"
+
+    val CLOCKED_IN: String = "CLOCKED_IN"
+    val SITE_IMAGE_PATH = "SITE_IMAGE_PATH"
+    val CLOCKED_IN_TIME = "CLOCKED_IN_TIME"
+    val CLOCKED_OUT_TIME = "CLOCKED_OUT_TIME"
+    val SITE_CITY_NAME = "SITE_CITY_NAME"
+    val SHOOTS_SESSION = "SHOOTS_SESSION"
+    val IMAGE_SELECTED = "SELECTED_IMAGE"
+    val LOCALE = "LOCALE"
+
+    val IS_360: String = "IS_360"
+    val IS_HD: String = "IS_HD"
+
+    val FOOD_AND_BEV_CATEGORY_ID = "cat_Ujt0kuFxF"
+
+    val SHOOT_IMAGE_NAME_LIST = "SHOOT_IMAGE_NAME_LIST"
+    val LIST_IMAGE_NAME = "LIST_IMAGE_NAME"
+    val tokenId: String = "tokenId"
+    val CREDITS_MESSAGE = "CREDITS_MESSAGE"
+    val AUTH_KEY = "AUTH_KEY"
+    val UPLOAD_MORE = "UPLOAD_MORE"
+    val SKU_META_DATA = "SKU_META_DATA"
+    val IS_DOWNLOADED_BEFORE = "IS_DOWNLOADED_BEFORE"
+    val CREDIT_REMAINING = "credit_remaining"
+    val EXPOSURES: String = "exposures"
+    val WINDOWS: String = "windows"
+    val BG_ID: String = "bg_id"
+    val EMAIL_ID: String = "email_id"
+    val GIF_LIST: String = "gif_list"
+    val ALL_FRAME_LIST: String = "all_frame_list"
+    val ALL_IMAGE_LIST: String = "all_image_list"
+    val ALL_INTERIOR_IMAGE_LIST: String = "all_interior_image_list"
+    val ALL_INTERIOR_FRAME_LIST: String = "all_interior_frame_list"
+    val ALL_FOCUSED_IMAGE_LIST: String = "all_focused_image_list"
+    val ALL_FOCUSED_FRAME_LIST: String = "all_focused_frame_list"
+
+    val ALL_DEALERSHIP_CORNER_LIST: String = "all_dealership_corner_list"
+
+    val LIST_HD_QUALITY: String = "list_hd_quality"
+    val LIST_WATERMARK: String = "list_watermark"
+
+    val WATER_MARK_LIST: String = "water_mark_list"
+
+    val GIF: String = "gif"
+    val FRAME_LIST: String = "frame_list"
+    val FRAME_SHOOOTS: String = "frame_shoots"
+    val sku_incomplete: String = "sku-incomplete"
+    val completed: String = "completed"
+    val FROM_ACTIVITY: String = "from_activity"
+    val REPLACED_IMAGE: String = "replaced_image"
+    val IMAGE_ID: String = "image_id"
+    val BACKGROUND_LIST_CARS: String = "background_cars"
+    val SKU_COUNT: String = "sku_count"
+    val CHANNEL_COUNT: String = "channel_count"
+    val AMOUNT: String = "amount"
+    val BACKGROUND_LIST: String = "background_lits"
+    val CHANNEL_LIST: String = "channel_list"
+    val MY_LIST: String = "my_list"
+    val MY_LISTS: String = "my_lists"
+    val FROM: String = "from"
+    val LANGUAGE: String = "language"
+    val COLOR: String = "color"
+    val DESCRIPTION: String = "description"
+    val ONBOARD: String = "onboard"
+    val MAIN_IMAGE: String = "main_image"
+    val POSITION: String = "position"
+    val FRAME_IMAGE: String = "frame_image"
+    val FRAME: String = "frame"
+    val TOTAL_FRAME: String = "total_frame"
+    val PRODUCT_ID: String = "product_id"
+    val INDEX_NUMBER: String = "index_number"
+    val SKU_ID: String = "sku_id"
+    val SKU_UUID: String = "sku_uuid"
+    val SKU_NAME: String = "sku_name"
+    val IMAGE_TYPE: String = "image_type"
+    val IMAGE_URL: String = "image_url"
+    val IMAGE_URI: String = "image_uri"
+    val SHOOT_ID: String = "shopot_id"
+    val CATEGORY_NAME: String = "category_name"
+
+    //const val BASE_IMAGE_URL = "https://spyne-cliq.s3.ap-south-1.amazonaws.com/"
+    const val BASE_IMAGE_URL = "https://storage.googleapis.com/spyne-cliq/"
+    const val BASE_IMAGE_URL_2 = "https://storage.googleapis.com/"
+
+    val shootEvn = "shoot_evn"
+    val noPlateLogoId = "no_plate_logo_id"
+    val is360 = "is360"
+    val tintWindow = "tintWindow"
+    val plateLogo = "plateLogo"
+    val blurNoPlate = "blurNoPlate"
+    val windowCorrection = "windowCorrection"
+
+    val CATEGORY_ID = "category_id"
+    val SUB_CAT_ID = "sub_cat_id"
+    val IMAGE_FILE = "image_file"
+    val LOGO_FILE = "logo_file"
+    val RAW_IMAGE_FILE = "raw_image_file"
+    val phone: String = "number"
+    val TOKEN_ID: String = "tokenId"
+    val otp: String = "otp"
+
+    val highQualityCount: String = "highQualityCount"
+    val backgroundNumber = "backgroundNumber"
+    val MARKETPLACE_ID = "marketplace_id"
+    val BACKGROUND_COLOUR = "background_colour"
+
+    val NO_OF_IMAGES: String = "no_of_images"
+
+    val DOWNLOAD_TYPE: String = "download_type"
+
+    val CREDIT_AVAILABLE: String = "credit_available"
+    val CREDIT_USED: String = "credit_used"
+    val CREDIT_DECUCTED: String = "credit_deducted"
+    val CREDIT_ALLOTED: String = "credit_alloted"
+    val PRICE: String = "price"
+
+    val UPDATED_CREDIT: String = "updated_credit"
+
+    val DEALERSHIP_LOGO: String = "dealership_logo"
+
+    val CORNER_POSITION: String = "corner_position"
+
+
+    val USER_NAME: String = "userName"
+    val USER_EMAIL: String = "userEmail"
+
+    val VIDEO_URL: String = "videoUrl"
+
+    val IS_NEW_USER: String = "isNewUser"
+
+
+    //drafts constants
+    val FROM_DRAFTS: String = "fromDrafts"
+    val PROJECT_UUIID: String = "project_uuid"
+    val PROJECT_ID: String = "project_id"
+    val PROJECT_NAME: String = "projectName"
+    val SKU_CREATED: String = "skuCreated"
+    val EXTERIOR_ANGLES: String = "exteriorAngles"
+    val SUB_CAT_NAME: String = "SUB_CAT_NAME"
+    val RESUME_EXTERIOR = "RESUME_EXTERIOR"
+    val EXTERIOR_SIZE = "EXTERIOR_SIZE"
+    val MISC_SIZE = "MISC_SIZE"
+    val INTERIOR_SIZE = "INTERIOR_SIZE"
+    val RESUME_INTERIOR = "RESUME_INTERIOR"
+    val RESUME_MISC = "RESUME_MISC"
+    val FROM_LOCAL_DB = "FROM_LOCAL_DB"
+    val EXTERIOR_LIST = "EXTERIOR_LIST"
+
+
+    //app names
+    val KARVI = "Karvi Studio"
+    val UNIDAS = "Unidas Estudio"
+    val CARSCOLLECTIVE = "Cars Collective"
+    val CARS24_INDIA = "Cars24 (B2C) Cataloging"
+    val CARS24 = "Cars 24 Cataloging"
+    val SWEEP = "Sweep Photos"
+    val TRUSTED_CARS = "Trusted Cars"
+    val TRAVO_PHOTOS = "Travo Photos"
+    val YALLA_MOTOS = "Yalla Motors"
+    val OLA_CABS = "Ola Cabs"
+    val AUTO_FOTO = "AutoFotoApp"
+    val MEGADEALER = "MegaDealer"
+    val PIXLRPRO = "Pixlrpro"
+    val BRANDINI = "Brandini"
+    val SELL_ANY_CAR = "Sell any Car"
+    val PENSKE_AUTOMOTIVE = "Penske Automotive"
+    val CAR_DEKHO = "Car Dekho"
+    val CARWALE = "Carwale"
+    val LOTENHANCE = "LotEnhance"
+    val YOGACARS = "YogaCars Studio"
+    val EBAY = "Ebay"
+    val PARKPLUS = "Park+"
+    val FLIPKART_GROCERY = "Flipkart Grocery"
+    val CROWN_CARS = "Crown Cars"
+    val CARSOME = "Carsome"
+    val iCarrosFotos = "iCarros Fotos"
+    val FIAT = "Fiat"
+    val Carousell = "Carousell"
+    val SHRIRAMAUTOMALL = "Shriram Auto Mall"
+
+
+    val UDAAN = "Udaan"
+    val ZOOMCAR = "Zoomcar"
+    val CARLAXY = "Carlaxy"
+    val MFC = "MFC"
+    val Moladin = "Moladin Agent"
+    val SWIGGY = "ShootEzy"
+    val ZOMATO = "Zomato"
+    val FOODPANDA = "foodpanda"
+    val SWIGGYINSTAMART = "Swiggy Instamart"
+    val BATA = "Bata"
+    val FLIPKART = "Flipkart"
+    val AMAZON = "Amazon"
+    val ICARRO = "iCarros Fotos"
+
+
+    val SPYNE_AI = "SpyneAI"
+    val SPYNE_AI_AUTOMOBILE = "SpyneAI Automobile"
+
+    val REGULAR = "REGULAR"
+    val SKIPPED = "SKIPPED"
+
+    val START_FILES_WORKER = "START_FILES_WORKER"
+    val LAL_10 = "Lal10"
+    val DEVICE_MANUFACTURER = "DEVICE_MANUFACTURER"
+    val OS_VERSION = "OS_VERSION"
+    val APP_VERSION = "APP_VERSION"
+    val DEVICE_NAME = "DEVICE_NAME"
+    val NETWORK_TYPE = "NETWORK_TYPE"
+    val DEVICE_ID = "DEVICE_ID"
+    val MODEL = "MODEL"
+    val APP_VERSION_CODE = "APP_VERSION_CODE"
+
+    val FROM_VIDEO = "FROM_VIDEO"
+
+    val VIDEO_PATH = "VIDEO_PATH"
+    val MINIMUM_VIDEO_DURATION = 5
+    val DEFAULT_PRESIGNED_URL = "DEFAULT_PRESIGNED_URL"
+
+    val UPLOAD_TRIGGERED = "UPLOAD_TRIGGERED"
+
+    // val UPLOADING_RUNNING = "UPLOADING RUNNING"
+    val IMAGE_ID_NULL = "IMAGE ID NULL"
+    val CONNECTION_BREAK = "CONNECTION BREAK"
+    val ALL_UPLOADED_BREAK = "ALL UPLOADED BREAK"
+    val IMAGE_MODIFIER = "IMAGE MODIFIER"
+
+
+    //val PROJECT_SYNC_RUNNING = "PROJECT_SYNC_RUNNING"
+    val PROJECT_SYNC_TRIGGERED = "PROJECT_SYNC_TRIGGERED"
+
+    //val PROCESS_SKU_RUNNING = "PROCESS_SKU_RUNNING"
+    val PROCESS_SKU_PARENT_TRIGGERED = "PROCESS_SKU_PARENT_TRIGGERED"
+
+    val NOTOFIED_ONCE = "NOTOFIED_ONCE"
+    val LAST_NOTIFIED = "LAST_NOTIFIED"
+
+    val IMAGE_COUNT = "IMAGE_COUNT"
+
+    val imageCatNameMap = HashMap<String, String>().apply {
+        put(FOOD_AND_BEV_CATEGORY_ID, "Food")
+    }
+
+    val NUMBER_PLATE_ID = "NUMBER_PLATE_ID"
+    val NUMBER_PLATE_LOGO = "NUMBER_PLATE_LOGO"
+    val SKU_STATUS = "SKU_STATUS"
+
+    val QUEUED_DRAFT: String = "queued_draft"
+    val DRAFT: String = "draft"
+}
---
+++ app/src/main/java/com/foodtestapp/DemoActivity.kt
@@ -0,0 +1,140 @@
+package com.foodtestapp
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import com.foodtestapp.databinding.ActivityDemoBinding
+import com.spyneai.foodsdk.sdk.*
+
+class DemoActivity : AppCompatActivity(), Spyne.SkuListener {
+
+    private lateinit var binding: ActivityDemoBinding
+    val TAG = "DemoActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDemoBinding.inflate(layoutInflater)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        setContentView(binding.root)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.subcategory_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner
+            binding.spSubcategory.adapter = adapter
+        }
+
+        var skuId = intent.getStringExtra("sku_id")
+
+        binding.etUserId.setText("userId")
+
+        skuId?.let {
+            binding.tvSkuId.text = it
+        }
+
+
+        binding.tvSkuId.setOnClickListener {
+            val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+            val clip = ClipData.newPlainText("label", binding.tvSkuId.text)
+            clipboard.setPrimaryClip(clip)
+            Toast.makeText(this, "SkuId copied to clipboard", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnStart.setOnClickListener {
+            when {
+                binding.etUserId.text.isNullOrEmpty() -> binding.etUserId.error = "Enter User Id"
+                binding.etSku.text.isNullOrEmpty() -> binding.etSku.error = "Enter Sku Id"
+                binding.etProjectName.text.isNullOrEmpty() -> binding.etSku.error =
+                    "Enter project name"
+                binding.etSkuName.text.isNullOrEmpty() -> binding.etSku.error = "Enter Sku name"
+                else -> {
+                    val builder = Spyne.ShootBuilder(
+                        this,
+                        binding.etUserId.text.toString(),
+                        this,
+                    )
+                        .uniqueId(binding.etUserId.text.toString())
+                        .projectName(binding.etProjectName.text.toString())
+                        .skuName(binding.etSkuName.text.toString())
+                        .metaData(HashMap<String, Any>().apply {
+                            put("rid", "test rid")
+                        })
+                        .shootType(getShootType())
+                        .classifier(getClassifierOptions())
+                        .gyroMeter(getGyroMeterOptions())
+                        .environment(if (AppConstants.BASE_URL == "https://beta-api.spyne.xyz/") "TESTING" else "PRODUCTION")
+                        .shootLocation(binding.cbSku.isChecked)
+                        .imageLocation(binding.cbImage.isChecked)
+
+                    builder.subcategoryId(binding.spSubcategory.selectedItem.toString())
+
+                    val spyne = builder.build()
+
+                    spyne.start()
+                }
+            }
+        }
+
+        binding.btnReshoot.setOnClickListener {
+                when {
+                    binding.etUserId.text.isNullOrEmpty() -> binding.etUserId.error = "Enter User Id"
+                    else -> {
+                        val builder = Spyne.ShootBuilder(
+                            this,
+                            binding.etUserId.text.toString(),
+                            skuListener = this,
+                        )
+                            .spyneSkuId(skuId ?: binding.etReshootSku.text.toString())
+                            .gyroMeter(getGyroMeterOptions())
+                            .gyroMeterVar(binding.etGyroDelta.text.toString().toInt())
+                            .classifier(getClassifierOptions())
+                            //.outputPreview(getOutputPreview())
+                            .imageLocation(true)
+
+
+                        val spyne = builder.build()
+                        spyne.reshoot()
+                    }
+                }
+        }
+
+    }
+
+    private fun getGyroMeterOptions(): Gyrometer {
+        return if (binding.gCbRestrictive.isChecked) Gyrometer.RESTRICTIVE
+        else if (binding.gCbNonRestrictive.isChecked) Gyrometer.NON_RESTRICTIVE
+        else Gyrometer.OFF
+    }
+
+    private fun getClassifierOptions(): Classifier {
+        return if (binding.cCbRestrictive.isChecked) Classifier.RESTRICTIVE
+        else if (binding.cCbNonRestrictive.isChecked) Classifier.NON_RESTRICTIVE
+        else Classifier.OFF
+    }
+
+        private fun getShootType(): ShootType {
+            return if (binding.cbUpload.isChecked) ShootType.UPLOAD
+            else if (binding.cbShoot.isChecked) ShootType.SHOOT
+            else ShootType.BOTH
+        }
+
+        override fun onShootCompleted(skuId: String, isReshoot: Boolean) {
+        }
+
+    override fun onSkuCreated(skuId: String) {
+        binding.tvSkuId.text = skuId
+    }
+
+
+    }
